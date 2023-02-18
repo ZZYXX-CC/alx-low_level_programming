118f7b1b26790ec6c7f7545b399e318942606ad4
Samuel Oladipo <ichris58@hotmail.com>
2023-02-17 16:10:15 -0800

    C is fun

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints random alphabets in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+*File: 3-print_alphabets.c
+*Auth: Samuel Oladipo
+*/
+
+#include<stdio.h>
+
+/**
+ * main - main function
+ * Description: Prints lower and uppercase alphabets
+ * Return: Always 0
+*/
+int main(void)
+{
+	int ch, CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
+	putchar('\n');
+	return (0);
+}
