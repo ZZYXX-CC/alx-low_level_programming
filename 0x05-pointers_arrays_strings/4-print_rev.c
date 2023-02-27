fc4714d4f1e14b40cf1c2cba3360a7c08cecdd96
Samuel Oladipo <ichris58@hotmail.com>
2023-02-27 13:34:01 -0800

    array and pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: string to be reserved.
+ */
+void print_rev(char *s)
+{
+	int leng, i;
+
+	leng = 0;
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+	i = leng - 1;
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
